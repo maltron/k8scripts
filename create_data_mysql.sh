935689c9b3c5fc73804e19b6bf3571287569d6d1
Mauricio Leal <mauricio@redbook.local>
2020-06-01 13:46:21 -0300

    Split into provisioning the pod and inserting data into the pod

---
+++ create_data_mysql.sh
@@ -0,0 +1,2 @@
+oc cp database.sql $(oc get pods -o jsonpath='{.items[?(@.status.phase=="Running")].metadata.name}'):/opt/app-root/src
+oc exec -i $(oc get pods -o jsonpath='{.items[?(@.status.phase=="Running")].metadata.name}') -- mysql -u $(oc get secret/mysql -o jsonpath='{.data.database-user}' | base64 --decode) -p$(oc get secret/mysql -o jsonpath='{.data.database-password}' | base64 --decode)<database.sql
--- create_mysql.sh
+++ create_mysql.sh
@@ -1,6 +1,1 @@
 oc new-app mysql-ephemeral -p MYSQL_DATABASE=chapter04 -p MYSQL_ROOT_PASSWORD=maltron -p MYSQL_USER=mauricio -p MYSQL_PASSWORD=maltron
-sleep 4
-oc wait --for=condition=Ready pod --selector name=mysql
-sleep 3
-oc cp database.sql $(oc get pods -o jsonpath='{.items[?(@.status.phase=="Running")].metadata.name}'):/opt/app-root/src
-oc exec -i $(oc get pods -o jsonpath='{.items[?(@.status.phase=="Running")].metadata.name}') -- mysql -u $(oc get secret/mysql -o jsonpath='{.data.database-user}' | base64 --decode) -p$(oc get secret/mysql -o jsonpath='{.data.database-password}' | base64 --decode)<database.sql
