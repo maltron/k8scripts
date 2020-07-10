c5f3c2a2020721e9a75999981856f92122ca4db5
Mauricio "Maltron" Leal <maltron@gmail.com>
2020-07-10 15:14:39 -0300

    Replaced the database name for survey

--- mysql/create.sh
+++ mysql/create.sh
@@ -1,1 +1,1 @@
-oc new-app mysql-ephemeral -p MYSQL_DATABASE=chapter04 -p MYSQL_ROOT_PASSWORD=maltron -p MYSQL_USER=mauricio -p MYSQL_PASSWORD=maltron
+oc new-app mysql-ephemeral -p MYSQL_DATABASE=survey -p MYSQL_ROOT_PASSWORD=maltron -p MYSQL_USER=mauricio -p MYSQL_PASSWORD=maltron
